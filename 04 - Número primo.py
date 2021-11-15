5bbafb4bb3715ff9db7e488cb98848c02d9ad367
moacirescorcio <moacirescorcio@gmail.com>
2021-11-15 18:46:49 -0300

    Update 04 - Número primo.py

--- 04 - Número primo.py
+++ 04 - Número primo.py
@@ -1,9 +1,11 @@
 n = int(input())
+num = 1
 
 cont_divisores = 0
-for divisor in range(1,n+1):
-    if n % divisor == 0:
+while num <= n:
+    if (n % divisor) == 0:
         cont_divisores += 1 
+    num += 1
 
 if cont_divisores == 2:
     print(True)
